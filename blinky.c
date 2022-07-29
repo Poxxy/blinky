b4f631befc1af03161af1311e84b3979aa217382
Poxxy <41162160+Poxxy@users.noreply.github.com>
2022-07-29 14:12:11 -0400

    Add prime number blinks
    Now when program starts the LED will blink in prime numbers. That is, it goes blink blink (2) HOLD, blink blink blink (3) HOLD, blink blink blink blink blink (5) HOLD, etc

--- blinky.c
+++ blinky.c
@@ -1,5 +1,31 @@
 #include "pico/stdlib.h"
 
+void simpleSieve(bool mark[], int limit)
+{
+    // Create a boolean array "mark[0..limit-1]" and
+    // initialize all entries of it as true. A value
+    // in mark[p] will finally be false if 'p' is Not
+    // a prime, else true.
+    
+    for(int i = 0; i<limit; i++) {
+      mark[i] = true;
+    }
+  
+    // One by one traverse all numbers so that their
+    // multiples can be marked as composite.
+    for (int p=2; p*p<limit; p++)
+    {
+        // If p is not changed, then it is a prime
+        if (mark[p] == true)
+        {
+            // Update all multiples of p
+            for (int i=p*p; i<limit; i+=p)
+                mark[i] = false;
+        }
+    }
+}
+
+
 int main() {
 #ifndef PICO_DEFAULT_LED_PIN
 #warning blink example requires a board with a regular LED
@@ -13,19 +39,36 @@ int main() {
     const uint PIN_19 = 19;
     gpio_init(PIN_19);
     gpio_set_dir(PIN_19, GPIO_OUT);
-    
-    while (true) {
-        // On board LED pin
+
+    // Signal program is starting.
+    for (int i = 0; i < 3; i++) {
         gpio_put(LED_PIN, 1);
         sleep_ms(250);
         gpio_put(LED_PIN, 0);
         sleep_ms(250);
-
-        // Breadboard LED Pin using PIN 19 to power it.
-        gpio_put(PIN_19, 1);
-        sleep_ms(250);
-        gpio_put(PIN_19, 0);
-        sleep_ms(250);
     }
+    
+    int limit = 10;
+    bool mark[limit];
+
+    simpleSieve(mark, limit);
+
+    for (int p=2; p<limit; p++) {
+        if (mark[p] == true) {
+            for (int blinks = 0; blinks < p; blinks++) {
+                gpio_put(PIN_19, 1);
+                sleep_ms(250);
+                gpio_put(PIN_19, 0);
+                sleep_ms(250);
+            }
+            sleep_ms(1000);
+            gpio_put(PIN_19, 1);
+            sleep_ms(1000);
+            gpio_put(PIN_19, 0);
+            sleep_ms(250);
+        }
+        
+    }
+
 #endif
 }
